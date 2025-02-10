344f98af909c1e7f6147272a7670cc25d2f2112f
DongKyoung <oe1lolo@nate.com>
2025-02-10 11:19:01 +0900

    feat: 웹앱에 Adrop SDK 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { useEffect } from 'react';
+import AppKey from './adrop_service.json';
 import './App.css';
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    adrop.initialize(AppKey.app_key);
+  }, []);
+
   return (
     <>
       <h1 className="text-red-600">Hello World</h1>
     </>
   );
-}
+};
 
 export default App;
---
+++ src/react-app-env.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="react-scripts" />
+
+declare let adrop: {
+  initialize: (appKey: string) => void;
+  request: (unitId: string) => Promise<{
+    ad: string;
+    unit: string;
+    headline?: string;
+    body?: string;
+    extra?: { id: string; text: string }[];
+    profile?: { displayName: string; displayLogo: string; link: string };
+  }>;
+  setProperty: (key: string, value: any) => Promise<void>;
+  logEvent: (name: string, params?: Record<string, any>) => Promise<void>;
+};
